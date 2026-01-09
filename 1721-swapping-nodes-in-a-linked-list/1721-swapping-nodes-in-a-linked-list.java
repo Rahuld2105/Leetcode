2070e069d71f925f789a6f0993ca41b4a06fe7eb
Rahuld2105 <rahul.dhadade.scoe.comp@gmail.com>
2026-01-09 17:18:35 +0530

    Time: 1 ms (100%), Space: 118.8 MB (73.81%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -1,41 +1,23 @@
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
 
-        // 1️⃣ find size of linked list
-        int n = 0;
-        ListNode temp = head;
-        while (temp != null) {
-            n++;
-            temp = temp.next;
+        ListNode first = head;
+        for (int i = 1; i < k; i++) {
+            first = first.next;
         }
 
-        // 2️⃣ create array of size n+1 (1-based indexing)
-        int arr[] = new int[n + 1];
+        ListNode fast = first;
+        ListNode second = head;
 
-        // 3️⃣ store linked list values in array
-        temp = head;
-        int i = 1;
-        while (temp != null) {
-            arr[i] = temp.val;
-            i++;
-            temp = temp.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            second = second.next;
         }
 
-        // 4️⃣ swap kth from left and kth from right
-        int left = k;
-        int right = n - k + 1;
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
 
-        int swapTemp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = swapTemp;
-
-        ListNode temp1 = head;
-        for(int j=1;j<arr.length;j++){
-            temp1.val = arr[j];
-            temp1 = temp1.next;
-        }
         return head;
-
-        
     }
 }
